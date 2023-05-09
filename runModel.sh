fbda661417afa51dbb22234c5d3b537b97bd8e76
Prachi Jain <ppjain@umass.edu>
2023-05-09 20:11:53 +0000

    Updated AB KD loss

--- runModel.sh
+++ runModel.sh
@@ -8,7 +8,7 @@ current_date=$(TZ=America/New_York date +'%Y-%m-%d %H: 74M:%S')
 config_file_name=$(basename "$1" .yaml)
 
 # set log file name
-log_file_name="pretraining_${config_file_name}_${current_date}_INFONCE_AB_15_BS256.txt"
+log_file_name="pretraining_${config_file_name}_${current_date}_AB_15_BS256.txt"
 
 # run command and save output to log file
 python pretraining.py --path_configs "$1" --gpus 3 >&1 | tee logs/"$log_file_name"
